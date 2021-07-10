554ab027946d82b478dd0ebe5ea8a5a2d2c613ea
Nathan PODESTA <nathan.podesta@icloud.com>
2021-07-11 01:00:11 +0200

    memorized cell hit

--- src/main/java/fr/lernejo/navy_battle/BuildMap.java
+++ src/main/java/fr/lernejo/navy_battle/BuildMap.java
@@ -5,10 +5,16 @@ import java.util.*;
 public class BuildMap {
     private final String[] boxesOnMap = new String[]{ "A1","A2","A3","A4","A5","A6","A7","A8","A9","A10","B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","D1","D2","D3","D4","D5","D6","D7","D8","D9","D10","E1","E2","E3","E4","E5","E6","E7","E8","E9","E10","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10", "G1","G2","G3","G4","G5","G6","G7","G8","G9","G10","H1","H2","H3","H4","H5","H6","H7","H8","H9","H10","I1","I2","I3","I4","I5","I6","I7","I8","I9","I10","J1","J2","J3","J4","J5","J6","J7","J8","J9","J10"};
     public final List<Map<String, String>> myMap = new ArrayList<Map<String, String>>();
+    public final List<String> alreadyHit = new ArrayList<String>();
 
     public String getRandomBoxes() {
-        int rnd = new Random().nextInt(boxesOnMap.length);
-        return boxesOnMap[rnd];
+        String cell = "A1";
+        while (alreadyHit.contains(cell)) {
+            int rnd = new Random().nextInt(boxesOnMap.length);
+            cell = boxesOnMap[rnd];
+        }
+        setAlreadyHit(cell);
+        return cell;
     }
 
     public void makefirstboat(Map<String, String> myMap1) {
@@ -59,6 +65,8 @@ public class BuildMap {
         }
         return false;
     }
+
+    public void setAlreadyHit(String cell) { alreadyHit.add(cell); };
 
     public String isTouched(String cell) {
         for (Map<String, String> map : myMap) {
